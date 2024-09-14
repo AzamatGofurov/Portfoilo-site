d484d1aafaacb64c90665295318724d759d70332
Azamat Gofurov <azatrader77@gmail.com>
2024-09-14 17:31:48 +0900

    css-dars.html

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// JavaScript for toggling the navigation
+function toggleNav() {
+    var x = document.getElementById("navbar");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+  
