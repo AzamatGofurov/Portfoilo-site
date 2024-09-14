cffe65c159f9511fdf0944721d00cab50c791ced
Azamat Gofurov <azatrader77@gmail.com>
2024-09-15 00:38:33 +0900

    index.html

--- js/app.js
+++ js/app.js
@@ -1,10 +1,17 @@
-// JavaScript for toggling the navigation
-function toggleNav() {
-    var x = document.getElementById("navbar");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
+// JavaScript for the Navbar toggle functionality on smaller screens
+document.addEventListener('DOMContentLoaded', function() {
+  const navToggle = document.querySelector('.nav-toggle');
+  const navMenu = document.querySelector('.main-nav');
+
+  navToggle.addEventListener('click', function() {
+      navMenu.classList.toggle('show-nav');
+  });
+
+  // Disable animations on mobile devices
+  const mediaQuery = window.matchMedia('(max-width: 767px)');
+  if (mediaQuery.matches) {
+      document.querySelectorAll('.service-card, .blog-item').forEach(item => {
+          item.style.transition = 'none';
+      });
   }
-  
+});
